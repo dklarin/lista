3e49ae19d12b67ae3941f04665936ff32a43dba6
dklarin <danijel.klarin@gmail.com>
2020-08-18 19:26:13 +0200

    Updates

---
+++ precache-manifest.37fe32944b2094c3e2ca51943517f816.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d24b6336b952ce7c217800b3adf9c5a",
+    "url": "/lista/index.html"
+  },
+  {
+    "revision": "e9e10bee152e942976ae",
+    "url": "/lista/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3387092709e5c083a465",
+    "url": "/lista/static/js/2.6c066293.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lista/static/js/2.6c066293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e10bee152e942976ae",
+    "url": "/lista/static/js/main.abdf78c4.chunk.js"
+  },
+  {
+    "revision": "f5fddc5895fdffee514b",
+    "url": "/lista/static/js/runtime-main.49ce278a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lista/static/media/logo.5d5d9eef.svg"
+  }
+]);
